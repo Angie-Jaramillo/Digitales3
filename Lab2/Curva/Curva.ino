be088a1686b2e0c1b70cf5f94541b3d6d3dd0f33
Angie-Jaramillo <apaola.jaramillo@udea.edu.co>
2025-05-01 23:16:23 -0500

    Modificación codigo de curva

--- Lab2/Curva/Curva.ino
+++ Lab2/Curva/Curva.ino
@@ -1,92 +1,123 @@
-//----- CONFIGURACION DE LOS PINES ---------
-#define ENA (15)
-#define EN1 (14)
-#define EN2 (13)
-#define ENCODER_PIN (16)
+#define PIN_ENCODER           16
+#define PIN_PWM               15
+#define PULSOS_POR_VUELTA     20
 
-#define PulsosPorVuelta (20)
+#define FRECUENCIA_PWM        250
+#define RESOLUCION_PWM        255
 
-//---- CONFIGURACION DEL PWM ------------
-#define PWM_FREQ (500)          // Frecuencia en Hz
-#define PWM_RESOLUTION (8)      // Resolución del PWM
-#define PWM_CYCLE (150)         // Valor del ciclo de trabajo 0-255 --> 0%-100%
+#define ESCALON_PORCENTAJE    20      // % de incremento por escalón
 
+#define NUM_ESCALONES_SUBIDA  6       
+#define NUM_ESCALONES_BAJADA  5
+#define TOTAL_ESCALONES       11      // 0, 20, 40, 60, 80, 100, 80, 60, 40, 20, 0
+#define MUESTRAS_POR_ESCALON  500
 
-struct Data {
-  unsigned long time;   // Tiempo en milisengundos
-  float rpm;            // Velocidad del motor en ese punto
+const int MAX_MUESTRAS = TOTAL_ESCALONES * MUESTRAS_POR_ESCALON;
+
+struct Buffer {
+  unsigned long tiempo;
+  uint8_t valor_pwm;
+  float rpm;
 };
 
-const int bufferSize = 5;       // Cuantas muestras de rpm se guarda para el filtro de media movil
-Data dataBuffer[bufferSize];         // Inicialización del buffer
-int bufferIndex = 0;            // Contador para iterar sobre los elementos del buffer
+Buffer datos[MAX_MUESTRAS];
+int buffIndex = 0;
 
-volatile int contadorPulsos = 0;    ///< Contador de pulsos del encoder
-unsigned long tiempoAnterior = 0;   ///< Ultimo tiempo registrado
-
+volatile int conteo_pulsos = 0;
 
 void contarPulsos() {
-  contadorPulsos++;
+  conteo_pulsos++;
 }
 
-
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
 
-  // Inicialización de los pines del puente H
-  pinMode(EN1, OUTPUT);
-  digitalWrite(EN1, HIGH);
-  pinMode(EN2, OUTPUT);
-  digitalWrite(EN2, LOW);
+  pinMode(PIN_ENCODER, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(PIN_ENCODER), contarPulsos, RISING);
 
-  // Se escribe el pwm 
-  analogWriteFreq(PWM_FREQ);
-  analogWriteResolution(PWM_RESOLUTION);
-  analogWrite(ENA, PWM_CYCLE);
+  pinMode(PIN_PWM, OUTPUT);
+  analogWriteFreq(FRECUENCIA_PWM);
+  analogWriteResolution(RESOLUCION_PWM);
+  analogWrite(PIN_PWM, 0);
 
-  // Se inicializa el pin del encoder y la interrupción
-  pinMode(ENCODER_PIN, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(ENCODER_PIN), contarPulsos, RISING);
-
+  Serial.println("tiempo(ms),valor_pwm(%),RPM");
 }
 
 void loop() {
-  unsigned long tiempoActual = millis();
 
-  // Cada 1 segundo
-  if (tiempoActual - tiempoAnterior >= 1000) {
+  static unsigned long tiempo_inicio = millis();
+  static unsigned long tiempo_ultima_muestra = tiempo_inicio;
+  static unsigned long tiempo_inicio_escalon = tiempo_inicio;
+  static int escalon_actual = 0;
+
+  unsigned long tiempo_actual = millis();
+
+  // Muestreo cada 4ms
+  if (tiempo_actual - tiempo_ultima_muestra >= 4) {// && buffIndex < MAX_MUESTRAS
+
     noInterrupts();
-    float count = contadorPulsos;
-    contadorPulsos = 0;
+    int pulsos = conteo_pulsos;
+    conteo_pulsos = 0;
     interrupts();
 
-    float rpm = (count / (float)PulsosPorVuelta) * 60.0;
+    float pulsos_por_segundo = pulsos * (1000.0 / 4.0);
+    float rpm = (pulsos_por_segundo / PULSOS_POR_VUELTA) * 60.0;
+    unsigned long tiempo_rel = tiempo_actual - tiempo_inicio;
 
-    // Guardar en buffer
-    if (bufferIndex < bufferSize) {
-      dataBuffer[bufferIndex++] = {tiempoActual, rpm};
+    //Determinar valor PWM actual según subida o bajada
+    uint8_t porcentaje_pwm;
+    if (escalon_actual < NUM_ESCALONES_SUBIDA) {
+
+      porcentaje_pwm = escalon_actual * ESCALON_PORCENTAJE;
+
+    } else {
+
+      porcentaje_pwm = (TOTAL_ESCALONES - escalon_actual) * ESCALON_PORCENTAJE;
+
     }
 
-    tiempoAnterior = tiempoActual;
+    uint8_t valor_pwm = map(porcentaje_pwm, 0, 100, 0, RESOLUCION_PWM);
+    analogWrite(PIN_PWM, valor_pwm);
+
+    datos[buffIndex++] = {tiempo_rel, valor_pwm, rpm }; //Almacenar datos
+    tiempo_ultima_muestra = tiempo_actual;
   }
 
-  // Después de 5 datos (5 segundos), enviamos por serial
-  if (bufferIndex >= bufferSize) {
-    float sum = 0;
-    for (int i = 0; i < bufferSize; i++) {
-      // Serial.print("Tiempo (ms): ");
-      // Serial.print(dataBuffer[i].time);
-      // Serial.print(" | RPM: ");
-      // Serial.println(dataBuffer[i].rpm);
-      sum += dataBuffer[i].rpm;
+  // Cambiar escalón cada 2s
+  if (tiempo_actual - tiempo_inicio_escalon >= 2000) { //verifica si han pasado 2s desde que comenzó el escalón actual de PWM
+
+    escalon_actual++;
+    tiempo_inicio_escalon = tiempo_actual;
+
+    if (escalon_actual >= TOTAL_ESCALONES) {//Fin de la secuencia
+
+      analogWrite(PIN_PWM, 0);  // Apagar motor
+
+      // Enviar datos por serial
+      for (int i = 0; i < buffIndex; i++) {
+        Serial.print(datos[i].tiempo);
+        Serial.print(',');
+        Serial.print(datos[i].valor_pwm);
+        Serial.print(',');
+        Serial.println(datos[i].rpm);
+      }
+      while (true);  // Fin del programa
     }
+    else {
+      // Configurar PWM para el nuevo escalón
+      uint8_t porcentaje_pwm;
+      if (escalon_actual < NUM_ESCALONES_SUBIDA) {
 
-    Serial.print("RPM: ");
-    Serial.println(sum/bufferSize);
+        porcentaje_pwm = escalon_actual * ESCALON_PORCENTAJE;
 
+      } else {
 
-    bufferIndex = 0; // Reiniciar buffer
+        porcentaje_pwm = (TOTAL_ESCALONES - escalon_actual) * ESCALON_PORCENTAJE;
+
+      }
+      int valor_pwm = map(porcentaje_pwm, 0, 100, 0, RESOLUCION_PWM);
+      analogWrite(PIN_PWM, valor_pwm);
+  }
   }
 }
 
